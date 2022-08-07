85586a960458830641dbdd58d6ff9891ba0d06c6
Enno <enno@example.com>
2022-08-07 19:06:38 +0200

    cli

---
+++ cmd/sid/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/staD020/sid"
+)
+
+const Version = 0.1
+
+func main() {
+	flag.Parse()
+	multipleFiles := flag.NArg() > 1
+	for _, path := range flag.Args() {
+		f, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		s, err := sid.New(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if multipleFiles {
+			fmt.Printf("%q: ", path)
+		}
+		fmt.Println(s)
+	}
+}
